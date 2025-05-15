6e77996404b75a39670294134fab9ebc2328d11f
erhant <erhany96@gmail.com>
2025-05-15 16:16:44 +0300

    small mdns rfk

--- src/service/mdns.rs
+++ src/service/mdns.rs
@@ -1,6 +1,23 @@
 use mdns_sd::ServiceInfo;
 
 impl crate::DnetService {
+    /// The service type for `dnet` within mDNS.
+    ///
+    /// Can be used to browse for `dnet` services, e.g. in MacOS:
+    ///
+    /// ```sh
+    /// dns-sd -B _dnet._tcp.
+    /// ```
+    ///
+    /// or alternatively:
+    ///
+    /// ```sh
+    /// dns-sd -Q _dnet._tcp.local. PTR
+    /// ```
+    ///
+    /// Note that a service type always ends with either `._tcp.local.` or `._udp.local.` in mDNS.
+    pub const MDNS_SERVICE_TYPE: &'static str = "_dnet._tcp.local.";
+
     /// Registers a service with the given instance name and hostname.
     ///
     /// - `instance_name`: The name of the service instance, e.g. `worker-1`
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,7 +1,10 @@
+/// Core service functionality.
 mod core;
 pub use core::DnetService;
 
+/// Service properties, also used in mDNS as `TXT` records.
 mod properties;
 pub use properties::ServiceProperties;
 
+/// mDNS specific functions.
 mod mdns;
