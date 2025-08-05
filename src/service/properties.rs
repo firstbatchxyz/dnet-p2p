625cdbcbf38e36b070a53910921433b4f474bb4e
erhant <erhany96@gmail.com>
2025-08-05 12:15:55 +0300

    allow multiple cpus and gpus

--- src/service/properties.rs
+++ src/service/properties.rs
@@ -2,6 +2,34 @@ use mdns_sd::{IntoTxtProperties, TxtProperties};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DnetServiceCPUProperties {
+    /// Brand of the CPU.
+    pub brand: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DnetServiceGPUProperties {
+    /// Name of the GPU.
+    pub name: String,
+    /// Type of the GPU, e.g. "Integrated" or "Dedicated".
+    pub kind: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DnetServiceMemoryProperties {
+    /// Amount of available memory in RAM, in bytes.
+    ///
+    /// On top of `free_memory`, this is the amount of memory that can be re-used as well.
+    pub avail: u64,
+    /// Amount of free memory in RAM, in bytes.
+    ///
+    /// In Windows / FreeBSD this is the same as `avail`.
+    pub free: u64,
+    /// Total amount of memory in RAM, in bytes.
+    pub total: u64,
+}
+
 /// A collection of metrics about a service instance.
 ///
 /// Implement [`IntoTxtProperties`] so that these properties can be used
@@ -13,33 +41,15 @@ use std::collections::HashMap;
 /// We are not using `repr(C)` in particular, because we are interested in a hashmap
 /// where this struct is the value, and the keys are strings (peer ids). So the natural
 /// thing to do is to serialize this to a JSON string to pass via FFI.
+///
+/// TODO: Boolean fields are represented as `1` for `true` and `0` for `false` in the `TxtProperties`.
+/// However, as per RFC 6763, we could maybe simply omit them for `false` and put them without a value for `true`.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[repr(C)]
 pub struct DnetServiceProperties {
-    //------------ MEM ------------//
-    /// Amount of available memory in RAM, in bytes.
-    ///
-    /// On top of `free_memory`, this is the amount of memory that can be re-used as well.
-    pub mem_avail: u64,
-    /// Amount of free memory in RAM, in bytes.
-    ///
-    /// In Windows / FreeBSD this is the same as `mem_avail`.
-    pub mem_free: u64,
-    /// Total amount of memory in RAM, in bytes.
-    pub mem_total: u64,
-    //------------ CPU ------------//
-    /// Number of CPUs available to this service.
-    pub num_cpus: u32,
-    /// Brand of the CPU.
-    pub cpu_brand: String,
-    //------------ GPU ------------//
-    /// Number of GPUs available to this service.
-    pub num_gpus: u32,
-    /// Brand of the GPU.
-    pub gpu_brand: String,
-    /// Type of the GPU, e.g. "Integrated" or "Dedicated".
-    pub gpu_type: String,
-    //------------ ANY ------------//
+    pub mem: DnetServiceMemoryProperties,
+    pub cpus: Vec<DnetServiceCPUProperties>,
+    pub gpus: Vec<DnetServiceGPUProperties>,
+
     /// Whether this service is a manager or not.
     ///
     /// We only expect there to be a single manager in the local network.
@@ -67,35 +77,38 @@ impl DnetServiceProperties {
         instance: String,
         address: String,
     ) -> Self {
-        let gpu_adapters = gpuinfo.enumerate_adapters(wgpu::Backends::all());
-        let mut props = Self {
-            //------------ MEM ------------//
-            // these will be refreshed just below
-            mem_avail: 0,
-            mem_free: 0,
-            mem_total: 0,
-            //------------ GPU ------------//
-            num_gpus: gpu_adapters.len() as u32,
-            gpu_brand: gpu_adapters.first().map_or_else(
-                || "Unknown".to_string(),
-                |adapter| adapter.get_info().name.to_string(),
-            ),
-            gpu_type: gpu_adapters.first().map_or_else(
-                || "Unknown".to_string(),
-                |adapter| match adapter.get_info().device_type {
+        let gpus = gpuinfo
+            .enumerate_adapters(wgpu::Backends::all())
+            .into_iter()
+            .map(|adapter| DnetServiceGPUProperties {
+                name: adapter.get_info().name.to_string(),
+                kind: match adapter.get_info().device_type {
                     wgpu::DeviceType::Other => "Other".to_string(),
                     wgpu::DeviceType::IntegratedGpu => "Integrated".to_string(),
                     wgpu::DeviceType::DiscreteGpu => "Discrete".to_string(),
                     wgpu::DeviceType::VirtualGpu => "Virtual".to_string(),
                     wgpu::DeviceType::Cpu => "CPU".to_string(),
                 },
-            ),
-            //------------ CPU ------------//
-            num_cpus: 0,
-            cpu_brand: sysinfo
-                .cpus()
-                .first()
-                .map_or_else(|| "Unknown".to_string(), |cpu| cpu.brand().to_string()),
+            })
+            .collect();
+
+        let cpus = sysinfo
+            .cpus()
+            .iter()
+            .map(|cpu| DnetServiceCPUProperties {
+                brand: cpu.brand().to_string(),
+            })
+            .collect();
+
+        let mem = DnetServiceMemoryProperties {
+            avail: sysinfo.available_memory(),
+            free: sysinfo.free_memory(),
+            total: sysinfo.total_memory(),
+        };
+        let mut props = Self {
+            mem,
+            cpus,
+            gpus,
             //------------ ANY ------------//
             // cant be busy at the start
             is_busy: false,
@@ -112,70 +125,83 @@ impl DnetServiceProperties {
 
     /// Repopulates the properties with the given [`sysinfo::System`] instance.
     pub fn refresh_sysinfo(&mut self, sysinfo: &sysinfo::System) {
-        self.mem_avail = sysinfo.available_memory();
-        self.mem_free = sysinfo.free_memory();
-        self.mem_total = sysinfo.total_memory();
-        self.num_cpus = sysinfo.cpus().len() as u32;
+        self.mem.avail = sysinfo.available_memory();
+        self.mem.free = sysinfo.free_memory();
+        self.mem.total = sysinfo.total_memory();
     }
 }
 
 impl From<&TxtProperties> for DnetServiceProperties {
     fn from(props: &TxtProperties) -> Self {
+        // memory stuff, all are `u64`
+        let [mem_avail, mem_free, mem_total] = ["mem_avail", "mem_free", "mem_total"].map(|key| {
+            props
+                .get_property_val_str(key)
+                .and_then(|s| s.parse().ok())
+                .unwrap_or_default()
+        });
+
+        // CPU properties
+        let num_cpus = props
+            .get_property_val_str("num_cpus")
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(0);
+        let mut cpus = Vec::with_capacity(num_cpus as usize);
+        for i in 0..num_cpus {
+            let prefix = format!("cpu_{i}");
+            cpus.push(DnetServiceCPUProperties {
+                brand: props
+                    .get_property_val_str(&format!("{}.brand", prefix))
+                    .unwrap_or_default()
+                    .to_string(),
+            });
+        }
+
+        // GPU properties
+        let num_gpus = props
+            .get_property_val_str("num_gpus")
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(0);
+        let mut gpus = Vec::with_capacity(num_gpus as usize);
+        for i in 0..num_gpus {
+            let prefix = format!("gpu_{i}");
+            gpus.push(DnetServiceGPUProperties {
+                name: props
+                    .get_property_val_str(&format!("{}.name", prefix))
+                    .unwrap_or_default()
+                    .to_string(),
+                kind: props
+                    .get_property_val_str(&format!("{}.kind", prefix))
+                    .unwrap_or_default()
+                    .to_string(),
+            });
+        }
+
+        // strings
+        let [hostname, instance, address] = ["hostname", "instance", "address"].map(|key| {
+            props
+                .get_property_val_str(key)
+                .unwrap_or_default()
+                .to_string()
+        });
+
+        // booleans
+        let [is_manager, is_busy] =
+            ["is_manager", "is_busy"].map(|key| props.get_property_val_str(key).is_some());
+
         Self {
-            mem_avail: props
-                .get_property_val_str("mem_avail")
-                .and_then(|s| s.parse().ok())
-                .unwrap_or_default(),
-            mem_free: props
-                .get_property_val_str("mem_free")
-                .and_then(|s| s.parse().ok())
-                .unwrap_or_default(),
-            mem_total: props
-                .get_property_val_str("mem_total")
-                .and_then(|s| s.parse().ok())
-                .unwrap_or_default(),
-            is_manager: props
-                .get_property_val_str("is_manager")
-                .map(|s| s == "1")
-                .unwrap_or(false),
-            is_busy: props
-                .get_property_val_str("is_busy")
-                .map(|s| s == "1")
-                .unwrap_or(false),
-            hostname: props
-                .get_property_val_str("hostname")
-                .unwrap_or_default()
-                .to_string(),
-            instance: props
-                .get_property_val_str("instance")
-                .unwrap_or_default()
-                .to_string(),
-            address: props
-                .get_property_val_str("address")
-                .unwrap_or_default()
-                .to_string(),
-            //------------ CPU ------------//
-            num_cpus: props
-                .get_property_val_str("num_cpus")
-                .and_then(|s| s.parse().ok())
-                .unwrap_or(0),
-            cpu_brand: props
-                .get_property_val_str("cpu_brand")
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| "Unknown".to_string()),
-            //------------ GPU ------------//
-            num_gpus: props
-                .get_property_val_str("num_gpus")
-                .and_then(|s| s.parse().ok())
-                .unwrap_or(0),
-            gpu_brand: props
-                .get_property_val_str("gpu_brand")
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| "Unknown".to_string()),
-            gpu_type: props
-                .get_property_val_str("gpu_type")
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| "Unknown".to_string()),
+            mem: DnetServiceMemoryProperties {
+                avail: mem_avail,
+                free: mem_free,
+                total: mem_total,
+            },
+            hostname,
+            instance,
+            address,
+            cpus,
+            gpus,
+            is_manager,
+            is_busy,
         }
     }
 }
@@ -186,28 +212,37 @@ impl IntoTxtProperties for &DnetServiceProperties {
     /// Each key-value pair is converted to a string representation as `{key}={value}`
     /// and must not exceed 255 bytes in total length, as per [RFC 6763 Sec. 6](https://www.rfc-editor.org/rfc/rfc6763.html#section-6).
     fn into_txt_properties(self) -> TxtProperties {
-        let props = HashMap::from_iter(
+        let mut props = HashMap::from_iter(
             [
-                ("is_manager", self.is_manager.to_string()),
-                ("is_busy", self.is_busy.to_string()),
                 ("hostname", self.hostname.to_string()),
                 ("instance", self.instance.to_string()),
                 ("address", self.address.to_string()),
-                //------------ MEM ------------//
-                ("mem_avail", self.mem_avail.to_string()),
-                ("mem_free", self.mem_free.to_string()),
-                ("mem_total", self.mem_total.to_string()),
-                //------------ CPU ------------//
-                ("num_cpus", self.num_cpus.to_string()),
-                ("cpu_brand", self.cpu_brand.to_string()),
-                //------------ GPU ------------//
-                ("num_gpus", self.num_gpus.to_string()),
-                ("gpu_brand", self.gpu_brand.to_string()),
-                ("gpu_type", self.gpu_type.to_string()),
+                ("mem_avail", self.mem.avail.to_string()),
+                ("mem_free", self.mem.free.to_string()),
+                ("mem_total", self.mem.total.to_string()),
             ]
             // map keys to strings
             .map(|(k, v)| (k.to_string(), v)),
         );
+
+        // bools
+        if self.is_manager {
+            props.insert("is_manager".to_string(), String::default());
+        }
+        if self.is_busy {
+            props.insert("is_busy".to_string(), String::default());
+        }
+
+        // add CPU and GPU brands
+        props.insert("num_cpus".to_string(), self.cpus.len().to_string());
+        for (i, cpu) in self.cpus.iter().enumerate() {
+            props.insert(format!("cpu_{i}.brand"), cpu.brand.to_string());
+        }
+        props.insert("num_gpus".to_string(), self.gpus.len().to_string());
+        for (i, gpu) in self.gpus.iter().enumerate() {
+            props.insert(format!("gpu_{i}.name"), gpu.name.to_string());
+            props.insert(format!("gpu_{i}.kind"), gpu.kind.to_string());
+        }
 
         // check lengths, must not exceed 255 bytes
         for (key, value) in props.iter() {
@@ -215,17 +250,16 @@ impl IntoTxtProperties for &DnetServiceProperties {
                 log::warn!("Property {key}={value} exceeds 255 bytes");
             }
         }
-
         props.into_txt_properties()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use sysinfo;
     use wgpu::{Backends, Instance};
 
-    /// Prints the de
     #[test]
     fn test_gpu_detection() {
         let instance = Instance::new(&wgpu::InstanceDescriptor::default());
@@ -248,5 +282,21 @@ mod tests {
                 cpu.cpu_usage()
             );
         }
+    }
+
+    #[test]
+    fn test_properties() {
+        let sysinfo = sysinfo::System::new_all();
+        let gpuinfo = Instance::new(&wgpu::InstanceDescriptor::default());
+        let props = DnetServiceProperties::new(
+            &sysinfo,
+            &gpuinfo,
+            true,
+            "localhost".to_string(),
+            "test_instance".to_string(),
+            "127.0.0.1".to_string(),
+        );
+
+        println!("Service Properties: {:#?}", props.into_txt_properties());
     }
 }
